16d3593fc726e29c983d3235c72b95e13109c1d8
w3school-test <dheerajbishwaas@gmail.com>
2025-05-22 16:17:57 +0530

    Course Details page Done

--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getPaginatedCourse,createCourse,getPaginatedCourses,getCourseById,updateCourse,deleteCourseById} = require('../controllers/courseController');
+const { getCourse,getPaginatedCourse,createCourse,getPaginatedCourses,getCourseById,updateCourse,deleteCourseById} = require('../controllers/courseController');
 const { verifyTokenAndRole } = require('../middleware/authMiddleware');
 
 router.post('/create',verifyTokenAndRole([1]), createCourse);
 router.get('/getCourseById/:id',verifyTokenAndRole([1]), getCourseById);
+router.get('/getbyid/:id', getCourse);
 router.put('/update/:id',verifyTokenAndRole([1]), updateCourse);
 router.get('/list', getPaginatedCourses);
 router.delete('/:id', deleteCourseById);
