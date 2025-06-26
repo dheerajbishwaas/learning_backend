4c4a38bc0ba988a548bac9e93727532ba9d81e95
w3school-test <dheerajbishwaas@gmail.com>
2025-06-26 11:53:15 +0530

    Update limit of data transfer

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ const path = require('path');
 dotenv.config();
 
 const app = express();
-
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 // Middleware
 app.use(express.json());
 app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));  // Allow all origins (for development)
